181d01d3736950405cdc93b4fed85dcb02d60994
Sébastien GAILLARD <sebastien.gaillard@acedigitale.com>
2024-05-23 11:42:26 +0200

    WIP scenario tests

--- Election/Election.cs
+++ Election/Election.cs
@@ -2,6 +2,46 @@
 {
     public class Election
     {
+        private Dictionary<Candidate, int> votes = new Dictionary<Candidate, int>();
+        private bool isClosed = false;
 
+        public void AddVote(Candidate candidate)
+        {
+            if (isClosed)
+            {
+                throw new InvalidOperationException("The election is already closed.");
+            }
+            if (votes.ContainsKey(candidate))
+            {
+                votes[candidate]++;
+            }
+            else
+            {
+                votes.Add(candidate, 1);
+            }
+        }
+
+        public void CloseElection()
+        {
+            isClosed = true;
+        }
+
+        public Candidate CalculateWinner()
+        {
+            if (!isClosed)
+            {
+                throw new InvalidOperationException("Election is not yet closed.");
+            }
+
+            int totalVotes = votes.Values.Sum();
+            foreach (var pair in votes)
+            {
+                double percentage = (double)pair.Value / totalVotes * 100;
+                if (percentage > 50)
+                    return pair.Key;
+            }
+
+            return null; // No winner if no one has more than 50%
+        }
     }
 }
---
+++ SpecFlowElection/StepDefinitions/ElectionStepDefinitions.cs
@@ -0,0 +1,45 @@
+﻿using Election;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowElection.StepDefinitions
+{
+    [Binding]
+    public sealed class ElectionStepDefinitions
+    {
+        private Election.Election election = new Election.Election();
+        private Candidate winner;
+
+        [Given(@"a candidate has more than 50% of the vote")]
+        public void GivenACandidateHasMoreThanOfTheVote()
+        {
+            var candidate = new Candidate("Seb");
+            for (int i = 0; i < 60; i++)
+            {
+                election.AddVote(candidate);
+            }
+        }
+
+        [Given(@"the first round of the election is closed")]
+        public void GivenTheFirstRoundOfTheElectionIsClosed()
+        {
+            election.CloseElection();
+        }
+
+        [When(@"we check the result of the first round")]
+        public void WhenWeCheckTheResultOfTheFirstRound()
+        {
+            winner = election.CalculateWinner();
+        }
+
+        [Then(@"this candidate has won the election")]
+        public void ThenThisCandidateHasWonTheElection()
+        {
+            winner.Should().NotBeNull();
+            winner.Name.Should().Be("Seb");
+        }
+    }
+}
